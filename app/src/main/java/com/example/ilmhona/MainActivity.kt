5d61e30923d5bfc385d0a0ac2c46f746b8ed6829
najmidin <taekwondotv67@gmail.com>
2023-03-02 20:20:27 +0500

    initial commmit

---
+++ app/src/main/java/com/example/ilmhona/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.ilmhona
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.ilmhona.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    1f (switch.isChecked) {
+        ImageView.visibility = View.VISIBLE
+    } else {
+        ImageView visibility • View. INVISIBLE
+                switch. setOnClickListener { it: Viewl
+                    if (switch 1sChecked) {
+                        imageVlew .visibility = Vlew.VISIBLE
+                        ) else {
+                        ImageView.visibility • View. INVISIBLE
+  }
